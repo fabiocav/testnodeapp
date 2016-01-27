7c2f732c86dd9a626ae3e698633c7ded99a8d380
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2016-01-26 17:46:33 -0800

    Test

--- tables/mytable.js
+++ tables/mytable.js
@@ -4,5 +4,7 @@ var table = module.exports = require('azure-mobile-apps').table();
 // table.read(function (context) {
 //     return context.execute();
 // });
-
+table.read(function (context){
+   context.req.azureMobile.user.getIdentity() 
+});
 // table.read.use(customMiddleware, table.operation);
